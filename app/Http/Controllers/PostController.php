f524bb936258f80247a22e1a49520b5fe939d478
grebmorts <stromberg.miklos.j@student.uta.fi>
2017-04-08 05:13:57 +0300

    2
    2

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,141 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Http\Requests;
+
+use Illuminate\Http\Request;
+use App\Post;
+
+use Carbon\Carbon;
+
+
+
+class PostController extends Controller
+{
+  public function __construct()
+  {
+
+    $this->middleware('auth')->except(['index', 'show']);
+
+  }
+
+
+  public function index()
+  {
+
+    $posts = Post::latest()
+      ->filter(request(['month', 'year']))
+      ->paginate(10);
+      //->get();
+
+    return view('posts.index', compact('posts'));
+
+  }
+
+
+  public function show(Post $post)
+  {
+
+    return view('posts.show', compact('post'));
+
+  }
+
+
+  public function create()
+  {
+
+    return view('posts.create');
+  }
+
+
+  public function store(Request $request)
+  {
+
+    $title = $request->input('title');
+    $body = $request->input('body');
+    $user = $request->user();
+
+    $post = new Post;
+
+    $post->title = $title;
+    $post->body = $body;
+    $post->user_id = $user->id;
+
+    $post->save();
+
+    /*
+    $this->validate(request(),[
+      'title' => 'required',
+      'body' => 'required'
+    ]);
+
+    auth()->user()->publish(
+
+      new Post(request(['title', 'body']))
+
+    );
+    */
+
+    session()->flash('message', 'Postauksesi on julkaistu.');
+
+
+    return redirect('/');
+
+    /* tai */
+
+    //return redirect('/')->with('message', 'Your post has now been published.');
+
+  }
+
+
+  public function edit($id){
+
+    $post = Post::find($id);
+
+    return view('posts.edit')->with('post', $post);
+
+  }
+
+
+  public function update(Request $request){
+
+    //dd(\App\User::find(auth()->id())->posts());
+
+    $title = $request->input('title');
+    $body = $request->input('body');
+    $id = $request->input('id');
+    $user = $request->user();
+
+    $post = new Post;
+
+    $post->title = $title;
+    $post->body = $body;
+    $post->user_id = $user->id;
+    $post->id = $id;
+
+    Post::where('id', $id)
+      ->update(['body' => $body]);
+
+    Post::where('id', $id)
+      ->update(['title' => $title]);
+
+      
+    session()->flash('message', 'Postauksesi on päivitetty.');
+    return redirect('/');
+
+  }
+
+
+  public function destroy($id){
+
+    // delete
+      $post = Post::find($id);
+      $post->delete();
+
+      // redirect
+      session()->flash('message', 'Julkaisu poistettu!');
+      return redirect('/');
+
+  }
+
+}
---
+++ app/Http/routes.php
@@ -0,0 +1,77 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+/*
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::controllers([
+  'auth' => 'Auth\AuthController',
+  'password' => 'Auth\PasswordController'
+]);
+
+*/
+
+Route::get('/register', 'RegistrationController@create');
+Route::post('/register', 'Auth\AuthController@postRegister');
+
+Route::get('/login', ['as' => 'login', 'uses' => 'SessionController@create']);
+Route::post('/login', 'Auth\AuthController@postLogin');
+Route::get('/logout', 'SessionController@destroy');
+
+Route::get('/', 'PostController@index')->name('home');
+
+// onko kirjautunut?
+
+Route::group(['middleware' => 'auth'], function () {
+  Route::get('/posts/create', 'PostController@create');
+  Route::post('/posts', 'PostController@store');
+  Route::post('/posts/{post}/comments', 'CommentController@store');
+
+// guest
+
+
+Route::get('/posts/{post}', 'PostController@show');
+Route::get('/posts/tags/{tag}', 'TagController@index');
+
+
+
+
+
+});
+
+// admin-näkymä & pyynnöt
+
+Route::group(['middleware' => ['auth', 'adminOnly']], function () {
+  Route::get('/users', 'AdminController@index');
+  Route::get('/users/{user}', 'AdminController@show');
+  Route::post('users/{user}/delete', 'AdminController@destroy');
+  Route::post('users/{user}/moderate', 'AdminController@makeMod');
+  Route::post('users/{user}/demoderate', 'AdminController@unmakeMod');
+
+});
+
+// Vähän ankea toteutus halutulle toiminnallisuudelle != roolit
+
+Route::group(['middleware' => ['auth', 'modOnly']], function () {
+  Route::post('posts/{post}/edit', 'PostController@update');
+  Route::get('posts/{post}/edit', 'PostController@edit');
+  Route::post('posts/{post}/delete', 'PostController@destroy');
+
+
+});
+
+//Route::auth();
+//Route::get('/home', 'HomeController@index');
